531c582834fe9a86ac267837cad68c382e17addb
Samuel Odongo <samieodd@gmail.com>
2021-09-04 17:19:53 +0300

    Created classes for modules and materials

--- web/main.dart
+++ web/main.dart
@@ -3,3 +3,27 @@ import 'dart:html';
 void main() {
   querySelector('#output')?.text = 'Your Dart app is running.';
 }
+
+class Material {
+  String name;
+  String grade;
+  int amount;
+
+  Material(
+    this.name,
+    this.grade,
+    this.amount,
+  );
+}
+
+class Module {
+  String name;
+  String engineer;
+  List<Material> materials;
+
+  Module(
+    this.name,
+    this.engineer,
+    this.materials,
+  );
+}
