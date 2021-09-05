fca62344ba180585d4458770a9b9ae145504de31
Samuel Odongo <samieodd@gmail.com>
2021-09-05 06:32:25 +0300

    Parse json, compare mat list then append to page

--- web/main.dart
+++ web/main.dart
@@ -1,29 +1,91 @@
+import 'dart:convert';
 import 'dart:html';
 
+import 'data_json.dart';
+
 void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+  var button = querySelector('#calculate') as ButtonElement;
+  button.onClick.listen(read_textarea);
+}
+
+List<Material> generateMaterialList() {
+  var processed_data = json.decode(unprocessed);
+  var newlist = [Material('Test', 1, 'idk', 'idk')];
+  for (var mat in processed_data) {
+    newlist
+        .add(Material(mat['name'], mat['grade'], mat['section'], mat['kind']));
+  }
+  return newlist;
 }
 
 class Material {
   String name;
-  String grade;
-  int amount;
+  int grade;
+  String section;
+  String kind;
+  int? amount;
 
   Material(
     this.name,
     this.grade,
-    this.amount,
+    this.section,
+    this.kind,
   );
 }
 
-class Module {
-  String name;
-  String engineer;
-  List<Material> materials;
+Material? matched_material(String material_name) {
+  var material_list = generateMaterialList();
+  try {
+    for (var mat in material_list) {
+      if (mat.name == material_name) {
+        return mat;
+      }
+    }
+  } catch (e) {
+    print(e);
+  }
+}
 
-  Module(
-    this.name,
-    this.engineer,
-    this.materials,
-  );
+void read_textarea(Event event) {
+  var textarea = querySelector('#materials') as TextAreaElement;
+  var text = textarea.value;
+  var content = [];
+  if (text != null) {
+    content = text.split('\n');
+    populate_accordion(content);
+  }
+}
+
+void populate_accordion(List<dynamic> materials) {
+  var raw_accordion = querySelector('#raw-body') as DivElement;
+  var encoded_accordion = querySelector('#encoded-body') as DivElement;
+  var manufactured_accordion =
+      querySelector('#manufactured-body') as DivElement;
+
+  for (dynamic material in materials) {
+    if (!material.isEmpty) {
+      var split_string = material.split(':');
+      print(split_string);
+      var material_name = split_string[0];
+      var material_amount = int.parse(split_string[1]);
+      var matching_material = matched_material(material_name) as Material;
+      matching_material.amount = material_amount;
+      if (matching_material.kind == 'Encoded') {
+        encoded_accordion.children.add(ParagraphElement()
+          ..text = matching_material.name +
+              ': ' +
+              matching_material.amount.toString());
+      } else if (matching_material.kind == 'Manufactured') {
+        manufactured_accordion.children.add(ParagraphElement()
+          ..text = matching_material.name +
+              ': ' +
+              matching_material.amount.toString());
+      } else if (matching_material.kind == 'Raw') {
+        raw_accordion.children.add(ParagraphElement()
+          ..text = matching_material.name +
+              ': ' +
+              matching_material.amount.toString());
+      }
+    }
+  }
 }
